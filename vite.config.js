0effd8ae0e1e6593130c37eaab16507f9391a533
piyushkaushl <piyushkaushal239@gmail.com>
2024-05-22 07:36:02 +0530

    added required files

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base : "/Endsem-Project-Beyoung---EndSem-Projects-Beyoung---rbdzeh8zm15i/",
   plugins: [react()],
 })
